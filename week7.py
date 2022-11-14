b1344b5e4f35461da5490ac7bfc31a095af2a3a0
TZUHAN07 <111477348+TZUHAN07@users.noreply.github.com>
2022-11-14 15:53:54 +0800

    Update week7.py

--- week7.py
+++ week7.py
@@ -13,7 +13,7 @@ try:
     mydb = mysql.connector.connect(
         host="localhost",    # 主機名稱
         user="root",         # 帳號
-        password="j610114*",  # 密碼
+        password="",  # 密碼
         database='website'   # 資料庫名稱
     )
 except mysql.connector.Error as err:
